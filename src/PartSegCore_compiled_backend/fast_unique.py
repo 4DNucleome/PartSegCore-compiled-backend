666af0cce5a22b0d741af871f224a19b4328a28c
Grzegorz Bokota <bokota+github@gmail.com>
2024-01-16 18:52:04 +0100

    Add tests for fast uinique (#8)
    * add tests for fast uinique
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * explicit provide to use fallback in tests
    
    * add docstring
    
    * remove obsolete randomization
    
    * change elif to if
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/PartSegCore_compiled_backend/fast_unique.py
+++ src/PartSegCore_compiled_backend/fast_unique.py
@@ -3,19 +3,29 @@ import numpy as np
 from ._fast_unique import unique1d, unique2d, unique3d
 
 
-def label_unique(array, numpy_fallback=False):
-    try:
-        if array.ndim == 1:
-            return unique1d(array)
-        if array.ndim == 2:
-            return unique2d(array)
-        elif array.ndim == 3:
-            return unique3d(array)
-        elif numpy_fallback:
-            return np.unique(array)
-        raise RuntimeError('Array must be 2d or 3d')
-    except TypeError:
-        if numpy_fallback:
-            return np.unique(array)
-        else:
-            raise
+def label_unique(array, numpy_fallback=False) -> np.ndarray:
+    """
+    Calculate unique values in array.
+
+    Parameters
+    ----------
+    array : np.ndarray
+        array to calculate unique values
+    numpy_fallback : bool
+        if True allow to use numpy.unique if cython version is not available
+        otherwise raise RuntimeError
+
+    Returns
+    -------
+    np.ndarray
+        array of unique values
+    """
+    if array.ndim == 1:
+        return unique1d(array)
+    if array.ndim == 2:
+        return unique2d(array)
+    if array.ndim == 3:
+        return unique3d(array)
+    if numpy_fallback:
+        return np.unique(array)
+    raise RuntimeError('Array must be 1d, 2d or 3d')
